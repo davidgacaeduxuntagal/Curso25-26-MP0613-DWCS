761085205c692219415f96e1e0b460ee91a9d18c
David García Cancio <davidgaca@edu.xunta.gal>
2026-03-12 14:38:00 +0100

    C#23 Cambio menor en TEMA-08

--- TEMA-08/claves.inc.php
+++ TEMA-08/claves.inc.php
@@ -1,10 +1,12 @@
 <?php
 // AÑADE AQUÍ TUS PROPIAS CLAVES (estas claves son falsas)
 $keyAEMET = "nopormuchomadrugaramanecemastempranonopormuchomadrugaramanecemastempranonopormuchomadrugaramanecemastempranonopormuchomadrugaramanecemastempranonopormuchomadrugaramanecemastempranonopormuchomadrugaramanecemastemprano";
+$keyOpenCage        = 'ab343474738483748374837483488eff';
+$keyGeoapify        = '72186d98fdc64148af066ca6f07cba51';
+$keyOpenWeatherMap  = '888888888888888888888888888abcde';  
 
-$keyOpenWeatherMap  = '0101010100101001010100101001100';  
-
-$keyBing = "M$365M$365M$365M$365M$365M$365M$365M$365M$365";  
+// Obsoleto: Microsoft nos dejó colgados
+// $keyBing = "M$365M$365M$365M$365M$365M$365M$365M$365M$365";  
 
 // Claves para proyecto P08_04_04_05
 $googleClientId     = '8549557018098-wefjfdfjsdkevlfljclkvjclvjclj.apps.googleusercontent.com'; 
